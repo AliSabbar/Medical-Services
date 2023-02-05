a33033414c289550840096b395988f2b215480b9
AliSabbar <agog.emo@gmail.com>
2023-02-05 20:29:07 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Medical Services',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const DeleteThisClass(),
+    );
+  }
+}
+
+//! Delete The Class
+
+class DeleteThisClass extends StatelessWidget {
+  const DeleteThisClass({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
