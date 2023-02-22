aa8838ccbef3cd2355cdbd8975a50328e88b85c9
noor <noorsaadjabar@gmail.com>
2023-02-23 02:41:42 +0300

    add choice account screen & providing services screen

--- lib/settings/routes_manger.dart
+++ lib/settings/routes_manger.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:medical_services/screens/choiceAcc/choiceAcc.dart';
 import 'package:medical_services/screens/homeLayout/home_layout.dart';
 import 'package:medical_services/screens/homeScreen/home_screen.dart';
+import 'package:medical_services/screens/providingService/providingService.dart';
 import 'package:medical_services/screens/signinScreen/signin_screen.dart';
 import 'package:medical_services/screens/splashScreen/splash_screen.dart';
 import '../screens/otpScreen/otp_screen.dart';
@@ -14,6 +16,8 @@ class Routes {
   static const String signInScreen = "/signInScreen";
   static const String signUpScreen = "/signUpScreen";
   static const String otpScreen = "/otpScreen";
+  static const String ChoiceAccount = "/ChoiceAccount";
+  static const String ProvidingService = "/ProvidingService";
 }
 
 class RouteGenerator {
@@ -22,9 +26,9 @@ class RouteGenerator {
       case Routes.splashScreenRoute:
         return MaterialPageRoute(builder: (context) => const SplashScreen());
       case Routes.homeLayoutRoute:
-        return MaterialPageRoute(builder: (context) =>const HomeLayOut());
+        return MaterialPageRoute(builder: (context) => const HomeLayOut());
       case Routes.homeScreenRoute:
-        return MaterialPageRoute(builder: (context) =>const HomeScreen());
+        return MaterialPageRoute(builder: (context) => const HomeScreen());
       case Routes.signInScreen:
         return MaterialPageRoute(builder: (context) => const SignInScreen());
       case Routes.signUpScreen:
@@ -35,6 +39,11 @@ class RouteGenerator {
             builder: (context) => OtpScreen(
                   title: title,
                 ));
+      case Routes.ChoiceAccount:
+        return MaterialPageRoute(builder: (context) => const ChoiceAccount());
+      case Routes.ProvidingService:
+        return MaterialPageRoute(
+            builder: (context) => const ProvidingService());
       default:
         return unDefinedRoute();
     }
